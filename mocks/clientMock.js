8b38c90db733645be4160beb005cc89a9c1f586d
matheusjesse <matheus_jesse@hotmail.com>
2023-04-17 18:07:37 -0300

    chore: adicionando mocks de clientes

---
+++ mocks/clientMock.js
@@ -0,0 +1,62 @@
+const clientData = [
+    {
+        "id": 1,
+        "nome": "João da Silva",
+        "veiculos": [
+          {
+            "marca": "Volkswagen",
+            "modelo": "Gol",
+            "ano": 2018,
+            "placa": "ABC-1234"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "nome": "Maria Oliveira",
+        "veiculos": [
+          {
+            "marca": "Toyota",
+            "modelo": "Corolla",
+            "ano": 2019,
+            "placa": "DEF-5678"
+          },
+          {
+            "marca": "Honda",
+            "modelo": "Civic",
+            "ano": 2017,
+            "placa": "GHI-9012"
+          }
+        ]
+      },
+      {
+        "id": 3,
+        "nome": "Juliana Rodrigues",
+        "veiculos": [
+          {
+            "marca": "Chevrolet",
+            "modelo": "Onix",
+            "ano": 2020,
+            "placa": "JKL-3456"
+          }
+        ]
+      },
+      {
+        "id": 4,
+        "nome": "Ricardo Oliveira",
+        "veiculos": [
+          {
+            "marca": "Fiat",
+            "modelo": "Uno",
+            "ano": 2015,
+            "placa": "MNO-7890"
+          },
+          {
+            "marca": "Renault",
+            "modelo": "Sandero",
+            "ano": 2018,
+            "placa": "PQR-1234"
+          }
+        ]
+      }
+]
