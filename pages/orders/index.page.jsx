85e310b6d34316dc51821f6659c4c96adac456a5
matheusjesse <matheus_jesse@hotmail.com>
2023-04-18 04:30:39 -0300

    feat: criando a pagina de orders

---
+++ pages/orders/index.page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from './style';
+
+export { Page }
+
+function Page() {
+
+  return (
+    <Container>
+        <h1>Orders</h1>
+    </Container>
+  );
+}
---
+++ pages/orders/style.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: flex-start;
+    flex-wrap: wrap;
+`
+
+export default Container;
