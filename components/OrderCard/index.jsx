1d6e548850d9b4835e554af6dfe16e911a194f1d
matheusjesse <matheus_jesse@hotmail.com>
2023-04-18 05:08:15 -0300

    feat: tags do componente OrderCard

--- components/OrderCard/index.jsx
+++ components/OrderCard/index.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
-import Container from './style';
+import Container, {
+    Title,
+    ClientContainer,
+    ClientInfo,
+    OficinaContainer,
+} from './style';
 
-function OrderCard() {
+function OrderCard({data}) {
    
   return (
     <Container>
-        
+        <ClientContainer>
+            <Title>Cliente</Title>
+            <ClientInfo>{`Nome${data.name}`}</ClientInfo>
+            <ClientInfo>{`Carro${data.car}`}</ClientInfo>
+            <ClientInfo>{`Carro${data.year}`}</ClientInfo>
+        </ClientContainer>
+        <OficinaContainer>
+            <Title>Oficina</Title>
+            <span>{`Mecânico(a): ${data.mechanicName}`}</span>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Preço</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.parts.length > 0 && (
+                            data.parts.map((element) =>
+                                <tr key={element.name}>
+                                    <td>{element.name}</td>
+                                    <td>{`R$ ${element.price.toFixed(2)}`}</td>
+                                </tr>
+                            )
+                        )
+                    }
+                </tbody>
+            </table>
+            <span>{`Mão de obra: ${data.workCommission}`}</span>  
+            <span>{`Deiscrição Adicional: ${data.description}`}</span>     
+            <span>{`Total: R$ ${data.total.toFixed(2)}`}</span>
+        </ OficinaContainer>
     </Container>
   )
 }
--- components/OrderCard/style.jsx
+++ components/OrderCard/style.jsx
@@ -5,5 +5,30 @@ const Container = styled.div`
 
 `
 
+const ClientContainer = styled.div`
+
+`;
+
+const ClientInfo = styled.p`
+    
+`
+
+const Title = styled.span`
+
+`
+
+const OficinaContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+`;
+
 
 export default Container;
+
+export {
+    Title,
+    ClientContainer,
+    ClientInfo,
+    OficinaContainer,
+}
