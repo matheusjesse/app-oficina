f3503b43c462ae8e40e2e765401eef15ca5550f4
matheusjesse <matheus_jesse@hotmail.com>
2023-04-18 08:24:03 -0300

    style: estilo dos dois cards da pagina home

--- components/CarRepairCard/style.jsx
+++ components/CarRepairCard/style.jsx
@@ -9,19 +9,60 @@ const Container = styled.div`
     justify-content: center;
     flex-wrap: wrap;
     align-content: center;
-    border-radius: 22px;
-    border: 1px solid black;
+    border-radius: 12px;    
+    
     margin-right: 8px;
 
+    h1 {
+      color: white;
+    }
+    
+    ::before {
+      content: "";
+      position: absolute;
+      background: rgb(71,9,121);
+      background: linear-gradient(90deg, rgba(71,9,121,1) 1%, rgba(61,69,158,1) 50%, rgba(130,168,251,1) 100%);   
+      border-radius: 12px;
+      filter: blur(1px);
+      opacity: 0.7;
+      z-index: -10;
+      width: 228px;
+      height: 308px;
+    }
+
+    ::after {
+      content: "";
+      position: absolute;
+      backdrop-filter: blur(5px);
+      -webkit-backdrop-filter: blur(5px);
+      border-radius: 12px;
+      z-index: -9;
+      width: 218px;
+      height: 290px;
+    }
+
+    :hover {
+      width: 228px;
+      height: 308px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-wrap: wrap;
+      align-content: center;
+      border-radius: 14px;    
+      box-shadow: -1px 0px 8px gray;
+      margin-right: 8px;
+    }
 `
 const PlusCircleIcon = styled(PlusCircle)`
-  color: black;
+  color: white;
   width: 40px;
   height: 40px;
 `
 
 const Title = styled.h1`
     text-align: center;
+    
 `
 
 export default Container;
